cfe10c5e8bef8c91f756fefb4aaaefedc8c165a5
Pranav Harer <pranavpharer@gmail.com>
2024-09-17 17:42:46 +0200

    abc

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 
 setup(name='Script_World',
       version='1',
+      packages=find_packages(),
       install_requires=['gym>=0.2.3',
                         'networkx==2.5.1']
-)
+) 
